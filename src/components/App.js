577207e3ee953aeefc24f3de73023a15a0884e8f
coldBlood <85475503+nileshhaz@users.noreply.github.com>
2025-03-24 10:41:07 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -6,27 +6,36 @@ class App extends Component {
         super(props)
         this.state = {
             renderBall: false,
-            posi : 0,
-            ballPosition: { left: "0px" }
+            posi : 5,
         };
         this.renderChoice = this.renderBallOrButton.bind(this)
         this.buttonClickHandler = this.buttonClickHandler.bind(this)
+	     this.onMove = this.onMove.bind(this);
     };
 
     buttonClickHandler() {
-   
+	   this.setState({renderBall:true})
    }
     renderBallOrButton() {
 		if (this.state.renderBall) {
-		    return <div className="ball" style={this.state.ballPosition}></div>
+		    return <div className="ball" style={{left:this.state.posi}}></div>
 		} else {
-		    return <button onClick={this.buttonClickHandler} >Start</button>
+		    return <button className="start" onClick={this.buttonClickHandler} >Start</button>
 		}
     }
+onMove() {
+    this.setState({ posi:this.state.posi+5 });
+  }
 
     // bind ArrowRight keydown event
     componentDidMount() {
-      
+	    const onMove = this.onMove
+       window.addEventListener("keydown", function (e) {
+      const key = e.key;
+      if (key === "ArrowRight") {
+        onMove();
+      }
+    });
     }
 
     render() {
